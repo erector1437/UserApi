664cce798e95c974727989569274b1ec6dac3a5e
Eric Rector <119783@nelnet.net>
2024-10-28 09:59:16 -0400

    updates for logging errors

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -28,16 +28,25 @@ namespace UserApi.Controllers
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
             var users = await _userService.GetUsersAsync();
-            return Ok(users.Select(user => new
+            if (users.Any())
             {
-                user.Id,
-                user.FirstName,
-                user.LastName,
-                user.Email,
-                user.DateOfBirth,
-                Age = CalculateAge(user.DateOfBirth),
-                user.PhoneNumber
-            }));
+                return Ok(users.Select(user => new
+                {
+                    user.Id,
+                    user.FirstName,
+                    user.LastName,
+                    user.Email,
+                    user.DateOfBirth,
+                    Age = CalculateAge(user.DateOfBirth),
+                    user.PhoneNumber
+                }));
+            }
+            else 
+            {                 
+                _logger.LogWarning("No users found.");
+                return NotFound();
+            }
+           
         }
 
         // GET: api/Users/5
@@ -71,17 +80,28 @@ namespace UserApi.Controllers
             ValidationResult result = await _validator.ValidateAsync(user);
             if (!result.IsValid)
             {
+                _logger.LogWarning("Validation failed for user: {User}", user);
                 return BadRequest(result.Errors);
+                
             }
 
             var existingUser = await _userService.GetUsersAsync();
             if (existingUser.Any(u => u.Email == user.Email))
             {
+                _logger.LogWarning("Validation failed for user: {User}", user);
                 return BadRequest("Email must be unique.");
+                
             }
-
-            await _userService.CreateUserAsync(user);
-            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+            try
+            {
+                await _userService.CreateUserAsync(user);
+                return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning("Post failed for user: {User}", user);
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT: api/Users/5
@@ -90,12 +110,14 @@ namespace UserApi.Controllers
         {
             if (id != user.Id)
             {
+                _logger.LogWarning("Validation failed for user: {User}", user);
                 return BadRequest();
             }
 
             ValidationResult result = await _validator.ValidateAsync(user);
             if (!result.IsValid)
             {
+                _logger.LogWarning("Validation failed for user: {User}", user);
                 return BadRequest(result.Errors);
             }
 
@@ -112,6 +134,7 @@ namespace UserApi.Controllers
                 }
                 else
                 {
+                    _logger.LogWarning("Put failed for user: {User}", user);
                     throw;
                 }
             }
